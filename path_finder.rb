7f5a2a055a98820c587c8f0c260dd76842982abe
DeBattlinBeen <cjbeenham@gmail.com>
2018-11-29 20:33:06 +0000

    Added Statement.txt, cleaned up some code

--- path_finder.rb
+++ path_finder.rb
@@ -4,15 +4,9 @@ class PathFinder
   end
 
   def find_all_paths(stop_id)
-    paths = []
-    next_stops = @bus_routes.find_next_stops(stop_id)
-    index = 0
-    next_stops.each do |s|
-      new_path = [stop_id,s]
-      paths[index] = new_path
-      create_path(paths,index,s)
-      index = paths.size
-    end
+    paths = [[stop_id]]
+    create_path(paths, paths.size-1, stop_id)
+
     str = "All paths starting from stop #{stop_id}\n"
     paths.each do |path|
       str += "#{path.to_s}\n"
@@ -22,26 +16,20 @@ class PathFinder
 
   def create_path(paths, index, stop_id)
     next_stops = @bus_routes.find_next_stops(stop_id)
-    if next_stops.size == 0
-      paths[index]
-    else
-      i = 0
+
+    unless next_stops.size == 0
       path = Array.new(paths[index])
       next_stops.each do |s_id|
-        if i == 0
+        if s_id == next_stops[0]
           paths[index] << s_id
           create_path(paths, index, s_id)
-          i += 1
         else
           path_1 = Array.new(path)
           path_1 << s_id
           paths << path_1
-          index = paths.size-1
-          create_path(paths,index,s_id)
-          i += 1
+          create_path(paths, paths.size-1, s_id)
         end
       end
-      paths
     end
   end
 end
